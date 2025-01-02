0ce3bb27f2b4e5d154ef3ef948c2db72d016438b
Andrey Strelnikov <xoz9ih@outlook.com>
2025-01-02 19:08:42 +0300

    fix carusel

--- src/components/carousel/carousel.tsx
+++ src/components/carousel/carousel.tsx
@@ -1,5 +1,29 @@
 import { CarouselUI } from '@ui/carousel';
+import { useEffect, useState } from 'react';
 
 export const Carousel: React.FC = () => {
-  return <CarouselUI />;
+  const words = [
+    'технологии.',
+    'код.',
+    'новые проекты.',
+    'разработку.',
+    'логику.',
+    'интерактив.',
+    'реализовывать.',
+  ];
+
+  const [currentWordIndex, setCurrentWordIndex] = useState(0);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCurrentWordIndex((prevIndex) => (prevIndex + 1) % words.length);
+    }, 3000);
+
+    return () => clearInterval(interval);
+  }, [words.length]);
+
+  const getWordByIndex = (offset: number) =>
+    words[(currentWordIndex + offset + words.length) % words.length];
+
+  return <CarouselUI getWordByIndex={getWordByIndex} />;
 };
--- src/components/ui/carousel/carousel.tsx
+++ src/components/ui/carousel/carousel.tsx
@@ -1,10 +1,34 @@
+import clsx from 'clsx';
 import styles from './carousel.module.scss';
-export const CarouselUI: React.FC = () => {
+
+interface CarouselUIProps {
+  getWordByIndex: (offset: number) => string;
+}
+
+export const CarouselUI: React.FC<CarouselUIProps> = ({ getWordByIndex }) => {
   return (
-    <section id="carousel" className={styles.carousel}>
-      <div className={styles.carousel__textContainer}>
-        <span className={styles.carousel__text}>я люблю разработку</span>
+    <div id="carousel" className={styles.container}>
+      <div className={styles.carousel}>
+        <div className={clsx(styles.carousel__wordContainer, styles.left)}>
+          <span className={styles.carousel__word}>{getWordByIndex(-2)}</span>
+          <span className={styles.carousel__word}>{getWordByIndex(-1)}</span>
+        </div>
+
+        <div className={clsx(styles.carousel__wordContainer, styles.center)}>
+          <span className={styles.carousel__fixedText}>
+            Я люблю
+            <span className={styles.carousel__wordShow}>
+              {getWordByIndex(0)}
+            </span>
+          </span>
+        </div>
+
+        <div className={clsx(styles.carousel__wordContainer, styles.right)}>
+          <span className={styles.carousel__word}>{getWordByIndex(1)}</span>
+          <span className={styles.carousel__word}>{getWordByIndex(2)}</span>
+          <span className={styles.carousel__word}>{getWordByIndex(3)}</span>
+        </div>
       </div>
-    </section>
+    </div>
   );
 };

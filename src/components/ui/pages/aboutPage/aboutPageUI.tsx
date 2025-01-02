a55f0d1ba9e27ae24c348101b145fe51dc9f2d12
Andrey Strelnikov <xoz9ih@outlook.com>
2025-01-02 10:14:16 +0300

    building message

--- src/components/ui/pages/aboutPage/aboutPageUI.tsx
+++ src/components/ui/pages/aboutPage/aboutPageUI.tsx
@@ -3,9 +3,10 @@ import style from '@styles/components/profilePageUI.module.scss';
 export const AboutPageUI: React.FC = ({}) => {
   return (
     <div className={style.container}>
-      <h2 className={style.container__title}>обо мне</h2>
+      <h1 className={style.container__name}>Cайт в разработке</h1>
+      {/* <h2 className={style.container__title}>обо мне</h2>
       <h2 className={style.container__subtitle}>технологии</h2>
-      <h2 className={style.container__description}>контакты</h2>
+      <h2 className={style.container__description}>контакты</h2> */}
     </div>
   );
 };

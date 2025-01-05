17389e1d5d9bbf9eb64965b8f70b571868f3b173
Andrey Strelnikov <xoz9ih@outlook.com>
2025-01-05 22:13:26 +0300

    fix lint

--- src/components/ui/pages/aboutPage/aboutPageUI.tsx
+++ src/components/ui/pages/aboutPage/aboutPageUI.tsx
@@ -6,12 +6,10 @@ interface AboutPageUIProps {
 export const AboutPageUI: React.FC<AboutPageUIProps> = ({ hanleClick }) => {
   return (
     <div className={style.container}>
-      <h1 className={style.container__name}>Cайт в разработке</h1>
+      <h1 className={style.container__name}>Страница в разработке</h1>
       <button className={style.container__button} onClick={hanleClick}>
-        обо мне
+        вернуться
       </button>
-      <h2 className={style.container__subtitle}>технологии</h2>
-      <h2 className={style.container__description}>контакты</h2>
     </div>
   );
 };

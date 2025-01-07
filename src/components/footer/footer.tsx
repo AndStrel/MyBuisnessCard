3986c0c9bc6c3e885f2bca9c58e83b155dbe54d6
Andrey Strelnikov <xoz9ih@outlook.com>
2025-01-07 13:49:47 +0300

    fix path

--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -3,7 +3,7 @@ import { FooterUI } from '@ui/footer';
 export const Footer: React.FC = () => {
   const downloadResume = () => {
     const link = document.createElement('a');
-    link.href = '/public/resume.pdf';
+    link.href = '/resume.pdf';
     link.download = 'resume.pdf';
     link.click();
   };

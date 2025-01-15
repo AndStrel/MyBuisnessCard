807f4797594a021e95886343db7767c22b8fe3ed
Andrey Strelnikov <xoz9ih@outlook.com>
2025-01-15 16:24:02 +0300

    add transition page

--- src/components/transition/transition.tsx
+++ src/components/transition/transition.tsx
@@ -1,9 +1,9 @@
 import { SwitchTransition, Transition } from 'react-transition-group';
 import { useLocation } from 'react-router-dom';
 import gsap from 'gsap';
-
 import { useAppDispatch } from '@utils/store';
 import { toggleCompleted } from '@slices/transitionSlice';
+import { useRef } from 'react';
 
 interface TransitionComponentProps {
   children: React.ReactNode;
@@ -14,35 +14,58 @@ export const TransitionComponent: React.FC<TransitionComponentProps> = ({
 }) => {
   const dispatch = useAppDispatch();
   const location = useLocation();
+  const nodeRef = useRef<HTMLDivElement>(null); // Создаем реф для DOM-узла
+
+  const handleOnEnter = () => {
+    const node = nodeRef.current;
+    if (node) {
+      dispatch(toggleCompleted(false));
+      gsap.set(node, {
+        autoAlpha: 0,
+        xPercent: location.pathname === '/about' ? 100 : -100,
+      });
+      gsap
+        .timeline({
+          paused: true,
+          onComplete: () => {
+            dispatch(toggleCompleted(true));
+          },
+        })
+        .to(node, {
+          autoAlpha: 1,
+          xPercent: 0,
+          duration: 0.5,
+          ease: 'power2.out',
+        })
+        .play();
+    }
+  };
+
+  const handleOnExit = () => {
+    const node = nodeRef.current;
+    if (node) {
+      gsap
+        .timeline({ paused: true })
+        .to(node, {
+          xPercent: location.pathname === '/about' ? 100 : -100,
+          autoAlpha: 0,
+          duration: 0.5,
+          ease: 'power2.in',
+        })
+        .play();
+    }
+  };
 
   return (
     <SwitchTransition>
       <Transition
         key={location.pathname}
         timeout={500}
-        onEnter={(node: gsap.TweenTarget) => {
-          dispatch(toggleCompleted(false));
-          gsap.set(node, { autoAlpha: 0, scale: 0.8, xPercent: -100 });
-          gsap
-            .timeline({
-              paused: true,
-              onComplete: () => {
-                dispatch(toggleCompleted(true));
-              },
-            })
-            .to(node, { autoAlpha: 1, xPercent: 0, duration: 0.25 })
-            .to(node, { scale: 1, duration: 0.25 })
-            .play();
-        }}
-        onExit={(node) => {
-          gsap
-            .timeline({ paused: true })
-            .to(node, { scale: 0.8, duration: 0.2 })
-            .to(node, { xPercent: 100, autoAlpha: 0, duration: 0.2 })
-            .play();
-        }}
+        nodeRef={nodeRef} // Передаем nodeRef
+        onEnter={handleOnEnter} // Вызываем обработчик
+        onExit={handleOnExit} // Вызываем обработчик
       >
-        {children}
+        {(state) => <div ref={nodeRef}>{children}</div>}
       </Transition>
     </SwitchTransition>
   );

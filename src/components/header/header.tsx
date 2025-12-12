9e929474e73ad8997d7a8e9cb592fe37817bcf67
Андрей Стрельников <andrey.strelnikov@infourok.biz>
2025-12-12 11:40:28 +0300

    on en lang and small fix

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,11 +1,16 @@
 import { HeaderUI } from '@ui/header';
+import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { useTranslation } from 'react-i18next';
 
 export const Header: React.FC = () => {
   const { i18n } = useTranslation();
+  const [selectedLanguage, setSelectedLanguage] = useState<'en' | 'ru'>(
+    (i18n.language as 'en' | 'ru') ?? 'en',
+  );
 
   const changeLanguage = (lng: 'en' | 'ru') => {
+    setSelectedLanguage(lng);
     i18n.changeLanguage(lng);
   };
 
@@ -13,5 +18,11 @@ export const Header: React.FC = () => {
   const navigateClick = () => {
     navigate('/about');
   };
-  return <HeaderUI hanleClick={navigateClick} language={changeLanguage} />;
+  return (
+    <HeaderUI
+      hanleClick={navigateClick}
+      language={changeLanguage}
+      selectedLanguage={selectedLanguage}
+    />
+  );
 };
--- src/components/ui/header/header.tsx
+++ src/components/ui/header/header.tsx
@@ -4,23 +4,40 @@ import { useTranslation } from 'react-i18next';
 interface HeaderUIProps {
   hanleClick: () => void;
   language: (lng: 'en' | 'ru') => void;
+  selectedLanguage: 'en' | 'ru';
 }
 
-export const HeaderUI: React.FC<HeaderUIProps> = ({ hanleClick, language }) => {
+export const HeaderUI: React.FC<HeaderUIProps> = ({
+  hanleClick,
+  language,
+  selectedLanguage,
+}) => {
   const { t } = useTranslation();
   return (
     <header className={styles.header}>
-      <div className={styles.header__logo}>front-end {t('profession')}</div>
-      <div className={styles.header__language}>
-        <button className={styles.button} onClick={() => language('en')}>
-          EN
-        </button>
-        <button className={styles.button} onClick={() => language('ru')}>
-          RU
-        </button>
+      <div className={clsx(styles.header__logo, styles.header__item)}>
+        front-end {t('profession')}
+      </div>
+      <div className={clsx(styles.header__language, styles.header__item)}>
+        {selectedLanguage !== 'en' && (
+          <button
+            className={clsx(styles.button, styles.header__item)}
+            onClick={() => language('en')}
+          >
+            EN
+          </button>
+        )}
+        {selectedLanguage !== 'ru' && (
+          <button
+            className={clsx(styles.button, styles.header__item)}
+            onClick={() => language('ru')}
+          >
+            RU
+          </button>
+        )}
       </div>
       <button
-        className={clsx(styles.button, styles.header__button)}
+        className={clsx(styles.button, styles.header__button, styles.header__item)}
         onClick={hanleClick}
       >
         {t('about')}
